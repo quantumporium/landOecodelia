725f2b43934a14effa57d4dd84478607be9b614b
quantumporium <quantumporium@email.com>
2022-10-16 19:17:45 -0700

    first commit: create readme and source/landOecodelia

---
+++ source/landOecodelia.c
@@ -0,0 +1,21 @@
+/*
+ * name 	: land of Ecodelia ( landOecodelia )
+ * author	: quantumporium	<quantumporium@email.com>
+ * date		: 16-10-2022
+ * description	: Text based reproduction of the game "land of Ecodelia" from Mr.Robot 
+ *
+ */
+
+
+
+
+
+
+
+
+
+// keep this at the botttom
+int main(){
+
+
+}
